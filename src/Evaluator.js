e4f173c988928d7b2ed45982d4b1e14c06df61dc
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-02-04 20:49:56 +0530

    DMAS priority calculations succesfull

--- src/Evaluator.js
+++ src/Evaluator.js
@@ -1,28 +1,91 @@
-let Evaluator = (exp) => {
-    let arr = exp.split(/([+|*|/|-])/);
-    console.log(arr);
-    while (arr.length != 1) {
-        let a = Number(arr.shift());
-        let operator = arr.shift();
-        let b = Number(arr.shift());
-        switch (operator) {
-            case '+':
-                arr.unshift(a + b);
-                break;
+let infixToPostFix = (infix) => {
+    let priority = {
+        '/': 3,
+        '*': 2,
+        '+': 1,
+        '-': 0
+    }
+
+    let postFix = [], operators = [];
+    for (let i = 0; i < infix.length; i++) {
+        switch (infix[i]) {
+
             case '-':
-                arr.unshift(a - b);
+                while (priority[operators[operators.length - 1]] >= priority['-'] && operators.length != 0) {
+                    postFix.push(operators.pop());
+                }
+                operators.push('-');
+                break;
+            case '+':
+                while (priority[operators[operators.length - 1]] >= priority['+'] && operators.length != 0) {
+                    postFix.push(operators.pop());
+                }
+                operators.push('+');
                 break;
             case '*':
-                arr.unshift(a * b);
+                while (priority[operators[operators.length - 1]] >= priority['*'] && operators.length != 0) {
+                    postFix.push(operators.pop());
+                }
+                operators.push('*');
+
                 break;
             case '/':
-                arr.unshift(a / b);
+                while (priority[operators[operators.length - 1]] >= priority['/'] && operators.length != 0) {
+                    postFix.push(operators.pop());
+                }
+                operators.push('/');
+
+                break;
+
+            default:
+                postFix.push(infix[i]);
                 break;
         }
     }
-    console.log(arr[0]);
-    return arr[0];
+    while (operators.length) {
+        postFix.push(operators.pop())
+    }
+    console.log(postFix)
+    // console.log(operators)
+    return postFix;
 }
-Evaluator('553-52/02*63+50');
+
+let postFixEvaluator = (postFix) => {
+    let answer = [];
+    for (let i of postFix) {
+        if (/[+|*|/|-]/.test(i)) {
+            console.log('The operator is : ', i)
+            console.log('The asn array is is : ', answer)
+            let b = Number(answer.pop());
+            console.log(b)
+            let a = Number(answer.pop());
+            console.log(a)
+            switch (i) {
+                case '+':
+                    answer.push(a + b);
+                    break;
+                case '-':
+                    answer.push(a - b);
+                    break;
+                case '*':
+                    answer.push(a * b);
+                    break;
+                case '/':
+                    answer.push(a / b);
+                    break;
+            }
+        } else {
+            answer.push(i);
+        }
+    }
+    // console.log("The answer is: ", answer);
+    return answer[0];
+}
+let Evaluator = (exp) => {
+    let arr = exp.split(/([+|*|/|-])/);
+    console.log(arr);
+    return postFixEvaluator(infixToPostFix(arr));
+}
+// Evaluator('553-52/02*63+50');
 
 export default Evaluator;
